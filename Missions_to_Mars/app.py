9adba671aa7aab24dbcfafaef824d94491449079
Paige Singleton <paigedsingleton@icloud.com>
2021-03-23 18:09:39 -0500

    HTML changes

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -25,10 +25,10 @@ def home():
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
+    scrape_data = scrape_mars.scrape_info()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.collection.update({}, scrape_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
